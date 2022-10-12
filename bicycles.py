d0c71a0ae697f78a2e98c075b99a1d64035be0a7
Username <daniellesmikle@Danielles-Air.home>
2022-10-11 23:11:51 -0400

    append

---
+++ bicycles.py
@@ -0,0 +1,36 @@
+bicycles= ['trek', 'cannole','redline','special']
+# print(bicycles)
+
+# print(bicycles[2].title())
+
+weaves= ['bob','ponytail','bangs','bodywave','burmese']
+# print(weaves[3].title())
+
+# print(weaves[1].upper())
+# print(weaves[1].title())
+
+# print(weaves[1])
+# print(weaves[3])
+# print(weaves[-1])
+# print(weaves[-2])
+# print(weaves[-3])
+
+# message='I think my first birthday wig will be a '  +weaves[1] + ' this year, i cannot wait!'
+# print(message)
+
+
+# new_mess= "It might be best for me to get " +weaves[2]+ ' this week since i got a new pimple on my forehead1 GRRRR'
+# print(new_mess)
+
+# message=(weaves[3].title()) + ' is always my go too hairstyle, eventhough it is so basic!'
+# print(message)
+
+
+# talk=' You should just go ahead and get a ' + weaves[-5].title() + ' you will still look like Lord Farquad'
+# print(talk)
+
+# message=('Yes Hunnay!! You got thisssssss')
+# print(message)
+
+message='Just cuz you decided to get a ' + weaves[0].title() + ' dont make you Bob Marley! You better go sit down'
+print(message)
---
+++ drinks.py
@@ -0,0 +1,45 @@
+# motor= ['honda',' yamaha', 'suzuki']
+
+# motor[0]= 'ducati'
+
+# print(motor)
+
+
+# drinks= ['sprite','coke','rebull','rum','pina colada']
+
+
+# drinks[0]= 'vodka'
+# print(drinks)
+
+# drinks[-2] ='ciroc boyz'
+# print(drinks)
+
+# drinks[-4]= 'hulk'
+# print(drinks)
+
+# drinks[0]='stomach ache='
+# print(drinks)
+
+# message= ' I had way too many ' + drinks[0] + 'last night, i cannot belive!'
+# print(message)
+
+
+# colors=['pink', 'orange', 'yellow', 'green', 'blue']
+# print(colors)
+
+# colors.append('red')
+# print(colors)
+
+# colors.append('black')
+# print(colors)
+
+# colors.append('purple')
+# print(colors)
+
+colors=[]
+
+colors.append('black')
+colors.append('orange')
+colors.append('pink')
+
+print(colors)
