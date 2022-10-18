1e8d2fcaf60c497d7bfff4e77f28c9ff49da8b97
Username <daniellesmikle@Danielles-Air.home>
2022-10-17 22:13:27 -0400

    popped

---
+++ breakfast.py
@@ -0,0 +1,36 @@
+breakfast= ['eggs', 'waffels', 'toast','hot coco','bacon']
+# print(breakfast)
+
+# breakfast.append('tea') # append just adds at the end, instert is where you can place a value anyehre 
+# print(breakfast)
+
+# breakfast.insert(4,'pancakes') # no brackets!
+# print(breakfast)
+
+# breakfast.insert(3,'strawberries')
+# print(breakfast)
+
+# breakfast.append('syrup')
+# print(breakfast)
+
+# breakfast.append('fish')
+# print(breakfast)
+
+# breakfast.insert(2,'wings')
+# print(breakfast)
+
+# popped_breakfast=breakfast.pop()
+# print(breakfast)
+
+# print(popped_breakfast)
+
+# del breakfast[4]
+# print(breakfast)
+
+# del breakfast[3]
+# print(breakfast)
+
+# print(popped_breakfast)
+
+last_ate= breakfast.pop()
+print('The last thing i ate was' + last_ate.title() + '.')
---
+++ food.py
@@ -0,0 +1,32 @@
+foods=['rice', 'beans', 'chilli', 'burgers']
+
+foods.append('tacos')
+
+# print(foods)
+
+
+foods.insert(4,'pizza')
+
+# print(foods)
+
+
+# del foods[3]
+# print(foods)
+
+# foods.insert(4,'milk')
+# print(foods)
+
+# foods.insert(2,'gum')
+# print(foods)
+
+# del foods[2]
+# print(foods)
+
+# popped_foods = foods.pop()
+# print(foods)
+
+# popped foods, helps to store the value of the item removed so it can be used again 
+
+# print(popped_foods)
+
+
