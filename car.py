ac734ea3368f3a353439d193c7570151d4d8fd08
Username <daniellesmikle@Danielles-Air.home>
2022-10-19 22:46:19 -0400

    remove

---
+++ car.py
@@ -0,0 +1,46 @@
+# cars= ['tesla','camero','jeep','lambo']
+
+# popped_cars = cars.pop()
+# # print(cars)
+
+# # print(popped_cars)
+
+
+# next_owned= cars.pop()
+
+# print('I cant wait until i purchase a' + next_owned + 'for myself!')
+
+# next_owned= cars.pop(1)
+# print('I love seeing a' + next_owned.title() +'in someones drive way!') # everytime you pop something its gone 
+
+# print(cars)
+
+# cars.append('porshe')
+# print(cars)
+
+# cars.insert(2,'benz')
+# print(cars)
+
+# cars.insert(0,'mercedez')
+# print(cars)
+
+# cars.pop(3)
+# print(cars)
+
+# makeup=['lashes','lip','gloss','foundation']
+# print(makeup)
+
+# makeup.remove('lip')
+# print(makeup)
+
+holidays=['halloween','christmas','easter','new years','flag day']
+
+not_real='flag day'
+
+holidays.remove(not_real)
+
+print('Why do yall even try and celebrate ' +  not_real +' anyways, it does not even count as a holiday')
+
+
+
+
