9581b8465f02a0a47d1f4e8967dcbe634077edbe
Username <daniellesmikle@Danielles-Air.home>
2022-10-24 21:50:47 -0400

    reverse

---
+++ pizza.py
@@ -0,0 +1,34 @@
+# toppings = ['pepperoni', 'cheese', 'sausage', 'spinach', 'chicken']
+
+
+# toppings.sort()
+# print(toppings)
+
+
+# toppings
+
+# toppings.sort(reverse=True)
+# print(toppings)
+
+# print('here is the original list:')
+# print(toppings)
+
+# print('\nHere is the sorted list:')
+# print(sorted(toppings))
+
+colors = ['green','blue','pink','yellow']
+
+# print('Here is the original list:')
+# print(colors)
+
+# print('\nHere is the sorted list:')
+# print(sorted(colors))
+
+# print('orignal again')
+# print(colors)
+
+# colors.reverse()
+# print(colors)
+
+len(colors)
+print(colors)
