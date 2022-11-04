86a2be738c7a2bde3ef1ce4b22ce6f67f4189aed
Username <daniellesmikle@Danielles-Air.home>
2022-11-03 23:30:27 -0400

    lists

---
+++ magician.py
@@ -0,0 +1,42 @@
+# for value in range(1,6):
+#     print(value)
+
+
+# numbers =list(range(1,6))
+# print(numbers)
+
+# even_numbers = list(range(2,11,2))
+# print(even_numbers)
+
+# squares= []
+# for value in range(1,11):
+#     squares.append(value**2)
+
+# print(squares)
+
+
+# digits =[1,2,3,4,5,6,7,8,9,0]
+# print(min(digits))
+
+# print(max(digits))
+
+# print(sum(digits))
+
+# squares =[value**2 for value in range(1,11)]
+# print(squares)
+
+# for value in range(1,22):
+#     print(value)
+
+# num= list(range(1,45))
+# # print(num)
+
+# print(min(num))
+# print(max(num))
+# print(sum(num))
+
+odd = list(range(1,31))
+print(odd)
+
+
+
---
+++ q3.py
@@ -0,0 +1,21 @@
+# num1= input("give input")
+# num2 = input("give input")
+
+# answer = int(num1) + int(num2)
+# print(answer, "is the sum")
+
+
+
+
+product= int("enter input") * int("enter input")
+
+print(product, "this is the answer")
+
+# difference= num1 - num2 
+
+# print(difference, "this is the answer")
+
+# quotient= num1/num2 
+
+# print( quotient)
+
