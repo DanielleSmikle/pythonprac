f61ee5686ef44766b69555cb8ff04f1b68ec48fc
Username <daniellesmikle@Danielles-Air.home>
2022-11-22 20:13:18 -0500

    fibinachi

--- recursion.py
+++ recursion.py
@@ -1,8 +1,19 @@
-import sys
-sys.setrecursionlimit(10000)
 
-def factorial(n):
-    print(n)
-    return n * factorial(n-1)
+# def factorial(n):
+#     assert n>=0 and int(n) ==n, 'The number must be postive int only'
+#     if n in [0,1]:
+#         return 1 
+#     else: 
+#         return n * factorial(n-1)
 
-factorial(3)
+# print(factorial(10))
+
+
+def fib(n):
+    assert n>=0 and int(n) ==n, 'fib number cant be neg number or non int like a decimal '
+    if n in [0,1]:
+        return n
+    else:
+        return fib(n-1) + fib(n-2)
+
+print(fib(5))
