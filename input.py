754479bcc5dd2bf2cbda304a0de548964f1668bc
Username <daniellesmikle@Danielles-Air.home>
2022-12-03 00:37:15 -0500

    input short

---
+++ input.py
@@ -0,0 +1,16 @@
+# message = input('say something and i will repeat it: ')
+# print(message)
+
+# name= input('Please enter your name: ')
+# print("Hello, " +name + "!")
+
+# promt = "if you tell us who you are I can make you a wig."
+# promt += '\nWhat is your first name?'
+
+# name = input(prompt)
+
+# print("\nHello, " + name + "!")
+
+
+age = input('how old are you? ')
+print(age)
